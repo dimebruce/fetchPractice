3951437c7a0430581751a544d80b5e0c7e7d55b6
unknown <127o1404@itsx.edu.mx>
2022-07-12 02:10:12 -0500

    Se hizo un template el cual se lleno con un fetch que se consume desde una API de rick and morty

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
-const api = 'https://jsonplaceholder.typicode.com/posts/'
+const cards = document.querySelector('#cards-dinamicas')
+const templateCards = document.querySelector('#template-cards').content
 
-// fetch(api)
-    //Primero la respuesta o response se tiene que formatear a JSON
-    // .then((resp) => resp.json())
-    // //Luego se manda a imprimir
-    // .then(data => console.log(data))
-    // //Se muestra un err si la ruta del api no existe
-    // .catch((err) => console.log(err))
-    // //Metodo a imprimir si o si cuando pasa por el then o catch
-    // .finally(() => console.log("El programa termino"))
+document.addEventListener("DOMContentLoaded", () => {
+    fetchData()
+})
 
-const findPostById = async (id) =>{
+ const fetchData = async () => {
+
+    //  console.log("funcionando")
     try {
-        const resp = await fetch(api + id)
+        laodingData(true)
+
+        const resp = await fetch("https://rickandmortyapi.com/api/character")
         const data = await resp.json()
-        console.log(data)
+        pintarCard(data)
+        //console.log(data)
     } catch (err) {
         console.log(err)
+    } finally {
+        laodingData(false)
     }
-}
+ }
+ 
+ const pintarCard = data => {
+    console.log(data)
+        //console.log(element)
+    const fragment = document.createDocumentFragment()
 
-findPostById(100)
+    data.results.forEach((element) => {
+    const clone = templateCards.cloneNode(true)
+    clone.querySelector("h5").textContent = element.name;
+    clone.querySelector("p").textContent = element.species;
+    clone.querySelector(".card-img").setAttribute("src", element.image);
+    fragment.appendChild(clone)
+    });
+    //Se usa el fragment para evitar el reflow -- Sustituye el inner
+    cards.appendChild(fragment)
+ }
+
+ const laodingData = estado => {
+     const loading = document.querySelector('#spinnerLoading')
+     if (estado) {
+         loading.classList.remove('d-none')
+     } else {
+        loading.classList.add('d-none')         
+     }
+ }
